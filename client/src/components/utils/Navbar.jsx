9cb044c97e628df1edbae1142feefc67a445b54f
Biswarup Ghosh <142293993+ImBiswarup@users.noreply.github.com>
2024-03-09 13:44:20 +0530

    Update Navbar.jsx

--- client/src/components/utils/Navbar.jsx
+++ client/src/components/utils/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom'; // Assuming you are using react-router-dom
+import { Link, useLocation } from 'react-router-dom'; 
 
 const Navbar = () => {
     const location = useLocation();
